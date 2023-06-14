814ac3204a91c25a3000e0a37b979566a7f94112
sadiq.alvi@visionet.com <sadiq.alvi@visionet.com>
2023-06-14 16:42:42 +0500

    Multiple routes for multiple file reads added

--- Muhammad Sadiq Alvi - 19369/Assignment-1/assignment-1.js
+++ Muhammad Sadiq Alvi - 19369/Assignment-1/assignment-1.js
@@ -3,8 +3,12 @@ const fs = require("fs");
 const os = require("os");
 
 const READ_FILE_PATH = "./TestFile.txt";
+const READ_FILE_PATH_1 = "./TestFile1.txt";
+const READ_FILE_PATH_2 = "./TestFile2.txt";
 const PORT = 3000;
 const READ_FILE_ROUTE = "/read-file";
+const READ_FILE_ROUTE_1 = "/read-file-1";
+const READ_FILE_ROUTE_2 = "/read-file-2";
 const server = http.createServer((request, response) => {   
     console.log(`Request url: ${request.url}`);
     console.log(`Reading File: ${READ_FILE_PATH}`);
@@ -15,6 +19,22 @@ const server = http.createServer((request, response) => {
             response.write(fileData);
         } catch (error) {
             console.log(`Unable to read file: ${READ_FILE_PATH}, Exception: ${error}`);
+        }
+    } else if(request.url == READ_FILE_ROUTE_1){
+        try {
+            const fileData = fs.readFileSync(READ_FILE_PATH_1, "utf8");
+            response.write(`File Contents:${os.EOL}`);
+            response.write(fileData);
+        } catch (error) {
+            console.log(`Unable to read file: ${READ_FILE_PATH_1}, Exception: ${error}`);
+        }
+    }  else if(request.url == READ_FILE_ROUTE_2){
+        try {
+            const fileData = fs.readFileSync(READ_FILE_PATH_2, "utf8");
+            response.write(`File Contents:${os.EOL}`);
+            response.write(fileData);
+        } catch (error) {
+            console.log(`Unable to read file: ${READ_FILE_PATH_2}, Exception: ${error}`);
         }
     } else {
         response.write(`Client Requested for route: ${request.url}`);
